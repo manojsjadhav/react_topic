efa07565ef5e64c034088876e9fc4a0cabe079fc
Manoj Jadhav <manojjadhav1097@gmail.com>
2024-02-28 14:33:30 +0530

    first commit on new branch

--- src/hooks/useContext/Counter.jsx
+++ src/hooks/useContext/Counter.jsx
@@ -6,9 +6,9 @@ const  {count, setCount} = useContext(countContaxt);
 
   return <div>
     <h1>counter</h1>
-    <button onClick={()=>setCount(count-1)}>dic</button>
+    <button onClick={()=>setCount(count-1)}>dicrease</button>
     <span style={{padding:"1rem"}}>{count}</span>
-    <button onClick={()=>setCount(count+1)}>inc</button>
+    <button onClick={()=>setCount(count+1)}>increase</button>
   </div>;
 };
 
